d9d9daec4caf70692cc031b4c51b3b10b50ed354
ChidoubReda <Reda.Chidoub@emsi-edu.ma>
2025-11-08 14:42:10 +0100

    Commit d'initialisation du TP3

---
+++ src/main/java/ma/emsi/chidoub/tp3_redachidoub/HelloApplication.java
@@ -0,0 +1,9 @@
+package ma.emsi.chidoub.tp3_redachidoub;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
